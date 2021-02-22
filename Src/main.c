f54bf557ceaf429a6010ec3ec3e819aaa2731e58
pixelsyntax <tom@thomaspurnell.com>
2021-02-22 13:23:30 +1000

    Update main.c
    Remove unneeded mmc comments

--- Src/main.c
+++ Src/main.c
@@ -1,19 +1,4 @@
 
-/*
-Bank-switching using the MMC3 mapper.
-We use a special linker config that sets up
-64 KB of PRG ROM and 64 KB of CHR ROM.
-Macros are used to set MMC3 registers and switch banks.
-CC65 #pragma directives are used to put things in various
-PRG ROM segments (CODE0-CODE6, CODE).
-*/
-
-// bank-switching configuration
-// #define NES_MAPPER 4        // Mapper 4 (MMC3)
-// #define NES_PRG_BANKS 4     // # of 16KB PRG banks
-// #define NES_CHR_BANKS 8     // # of 8KB CHR banks
-
-#include <peekpoke.h>
 #include <string.h>
 #include "neslib.h"
 
