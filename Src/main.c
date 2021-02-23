a92cfd9e1bb7734bfad51bbe1b2947f506189538
pixelsyntax <tom@thomaspurnell.com>
2021-02-23 12:34:09 +1000

    change to (non)mapper 0, no banks here shipmate

--- Src/main.c
+++ Src/main.c
@@ -1,26 +1,6 @@
-
-/*
-Bank-switching using the MMC3 mapper.
-We use a special linker config that sets up
-64 KB of PRG ROM and 64 KB of CHR ROM.
-Macros are used to set MMC3 registers and switch banks.
-CC65 #pragma directives are used to put things in various
-PRG ROM segments (CODE0-CODE6, CODE).
-*/
-
-// bank-switching configuration
-// #define NES_MAPPER 4        // Mapper 4 (MMC3)
-// #define NES_PRG_BANKS 4     // # of 16KB PRG banks
-// #define NES_CHR_BANKS 8     // # of 8KB CHR banks
-
-#include <peekpoke.h>
 #include <string.h>
 #include "neslib.h"
 
-// link the pattern table into CHR ROM
-//#link "chr_voxel.s"
-
-#define MMC_MODE 0x00
 #define TEXT_MSG "VOXEL'S ALAKAJAM 11 BASECODE"
 
 void main(void)
